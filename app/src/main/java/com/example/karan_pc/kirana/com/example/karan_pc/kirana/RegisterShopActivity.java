469bd0d4e9547fad4eaed86f609a630ab61f7b52
Karandev <karandevvj@gmail.com>
2015-06-20 13:29:31 +0530

    Third commit

--- app/src/main/java/com/example/karan_pc/kirana/com/example/karan_pc/kirana/RegisterShopActivity.java
+++ app/src/main/java/com/example/karan_pc/kirana/com/example/karan_pc/kirana/RegisterShopActivity.java
@@ -32,10 +32,15 @@ public class RegisterShopActivity extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_signup, container, false);
         btnRegisterShop = (Button)rootView.findViewById(R.id.btnLogin);
-        btnRegisterShop.setOnClickListener(v -> {
+        btnRegisterShop.setOnClickListener(new View.OnClickListener() {
 
-            BackgroundTask task = new BackgroundTask();
-            task.execute("Haha");
+            @Override
+            public void onClick(View v) {
+
+                BackgroundTask task = new BackgroundTask();
+                task.execute("Haha");
+            }
+
         });
 
         return rootView;
